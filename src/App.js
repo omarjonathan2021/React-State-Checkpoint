ede4fd6a7d5ba152eade825a86892b9576477157
Omar Jonathan <omarjonathan2017@gmail.com>
2024-01-12 10:10:03 +0100

    made some changes to the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Image from './Image/my image.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Jonathan Omar',
+        bio: 'A MERN Stack Developer, that Love to build things that Lives on the web.',
+        imgSrc: Image,
+        profession: 'Fullstack Developer',
+      },
+      show: false,
+      mountedTime: new Date(),
+    };
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({}), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, mountedTime } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          Toggle Profile
+        </button>
+        {show && (
+          <div className="content">
+          <div className='container'>
+            <div className='profiler'>
+              <h2>Name: {fullName}</h2>
+              <p>Bio: {bio}</p>
+              <p>Profession: {profession}</p>
+              <img style={{width:'7cm'}} src={imgSrc} alt="Image" />
+            </div>
+            <p>Time since mounted: {Math.round((new Date() - mountedTime) / 1000)} seconds</p>
+          </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
